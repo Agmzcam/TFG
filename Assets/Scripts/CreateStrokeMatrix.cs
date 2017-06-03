d27094ce66e8dd070454cffc2aceef5319f484c0
Agueda Gomez Cambronero <al285895@uji.es>
2017-06-03 14:01:37 +0200

    Permite dibujar y divide la pantalla en celdas correspondientes a una matriz de ceros guardando un uno por la celda en la que pase el ratón.

---
+++ Assets/Scripts/CreateStrokeMatrix.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateStrokeMatrix : MonoBehaviour {
+
+    private const int NCELLS = 3; // numero de filas y columnas de la matriz en la que se guardarán los datos y en las que se dividirá pantalla
+
+    private float cellsWidth = Screen.width / NCELLS; // ancho de cada celda
+    private float cellsHeigth = Screen.height / NCELLS; // alto de cada celda
+    private int row, column; // coordenadas columna, fila
+    private int[,] matrix = new int[NCELLS, NCELLS]; // matriz de datos del dibujo
+    private bool held;
+    private Camera cam;
+    private Vector3 mousePosition;
+    private Vector3 prevPosition;
+    private Vector3 endPosition;
+    private Mesh drawMesh;
+    private Material mat;
+    private Draw drawScript;
+
+    private void Start () {
+        cam = GetComponent<Camera>();
+        drawScript = GetComponent<Draw>();
+        drawMesh = new Mesh();
+        mat = new Material(Shader.Find("Sprites/Default"));
+        held = false;
+        InitializeMatrix();
+	
+	}
+
+    private void InitializeMatrix()
+    {
+        for (int r = 0; r < NCELLS; r++)
+        {
+            for (int c = 0; c < NCELLS; c++)
+            {
+                matrix[r,c] = 0;
+            }
+        }
+    }
+
+    private int Mousex2Column (float coorX)
+    {
+        return (int)(coorX / cellsWidth);
+    }
+
+    private int Mousey2Row (float coorY)
+    {
+        return (int)(NCELLS - coorY / cellsHeigth);
+    }
+
+    private void CreateDrawObject()
+    {
+        GameObject drawObject = new GameObject("DrawObject");
+        MeshFilter mf = drawObject.AddComponent<MeshFilter>();
+        mf.mesh = drawMesh;
+        MeshRenderer mr = drawObject.AddComponent<MeshRenderer>();
+        mr.material = mat;
+        drawObject.transform.position = Vector3.forward;
+
+        drawMesh = new Mesh();
+
+    }
+	
+	private void Update () {
+
+        if (Input.GetMouseButton(0))
+        {
+            mousePosition = Input.mousePosition;
+            row = Mousey2Row(mousePosition.y);
+            column = Mousex2Column(mousePosition.x);
+            //Debug.Log("Y = " + mousePosition.y + "--> " + row + " X = " + mousePosition.x + "--> " + column);
+
+            if (matrix[row, column] == 0)
+            {
+                matrix[row, column] = 1;
+            }
+
+            if (!held)
+            {
+                prevPosition = cam.ScreenToWorldPoint(Input.mousePosition);
+                held = true;
+            }
+            else
+            {
+                endPosition = cam.ScreenToWorldPoint(Input.mousePosition);
+                drawScript.AddLineSegmentToDrawMesh(drawMesh, prevPosition, endPosition);
+                prevPosition = endPosition;
+            }
+                
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            held = false;
+            CreateDrawObject();
+        }
+
+        Graphics.DrawMesh(drawMesh, Vector3.forward, Quaternion.identity, mat, 0);
+	}
+}
---
+++ Assets/Scripts/Draw.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draw : MonoBehaviour {
+
+    private float thickness = 0.1f; //grosor de la línea
+    private Vector3 a, b, c, d;
+    private Vector3 lineDir; //dirección de la línea
+    private Vector3 ortogonalLineDir; //vector ortogonal a la línea dibujada
+ 
+
+	private void Start () {
+       
+	}
+
+    public void CalculateVertexMexh (Vector3 start, Vector3 end)
+    {
+        a = start + ortogonalLineDir * thickness * 0.5f;
+        b = start - ortogonalLineDir * thickness * 0.5f;
+        c = end + ortogonalLineDir * thickness * 0.5f;
+        d = end - ortogonalLineDir * thickness * 0.5f;
+
+    }
+
+    public void AddLineSegmentToDrawMesh (Mesh currentMesh, Vector3 start, Vector3 end)
+    {
+        lineDir = end - start;
+        ortogonalLineDir = new Vector3(-lineDir.y, lineDir.x, lineDir.z).normalized;
+        if (lineDir.sqrMagnitude < 0.0001f)
+        {
+            return;
+        }
+
+        Vector3[] vertexList = currentMesh.vertices;
+        int vertexListLenght = vertexList.Length;
+        Vector3[] newVertexList;
+        if (vertexListLenght == 0)
+        {
+            newVertexList = new Vector3[vertexListLenght + 4];
+        }
+        else
+            newVertexList = new Vector3[vertexListLenght + 2];
+
+        for (int i = 0; i < vertexListLenght; i++)
+        {
+            newVertexList[i] = vertexList[i];
+        }
+        if (vertexListLenght != 0)
+        {
+            vertexListLenght -= 2;
+        }
+
+        newVertexList[vertexListLenght] = start + ortogonalLineDir * thickness * 0.5f;
+        newVertexList[vertexListLenght + 1] = start - ortogonalLineDir * thickness * 0.5f;
+        newVertexList[vertexListLenght + 2] = end + ortogonalLineDir * thickness * 0.5f;
+        newVertexList[vertexListLenght + 3] = end - ortogonalLineDir * thickness * 0.5f;
+
+        int[] triangleList = currentMesh.triangles;
+        int triangleListLength = triangleList.Length;
+        int[] newTriangleList = new int[triangleListLength + 6];
+        for (int i = 0; i < triangleListLength; i++) {
+            newTriangleList[i] = triangleList[i];
+        }
+        newTriangleList[triangleListLength] = vertexListLenght;     //a
+        newTriangleList[triangleListLength + 1] = vertexListLenght + 1; //b
+        newTriangleList[triangleListLength + 2] = vertexListLenght + 2; //c
+        newTriangleList[triangleListLength + 3] = vertexListLenght + 1; //b
+        newTriangleList[triangleListLength + 4] = vertexListLenght + 3; //d
+        newTriangleList[triangleListLength + 5] = vertexListLenght + 2; //c
+
+        currentMesh.Clear();
+        currentMesh.vertices = newVertexList;
+        currentMesh.triangles = newTriangleList;
+    }
+}
